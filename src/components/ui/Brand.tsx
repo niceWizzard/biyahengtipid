f13e55d875797d325c8bc58d698ab65f148d5e54
RIchard Manansala <richard232manansala@gmail.com>
2026-04-17 14:03:53 +0800

    feat: add link param in Brand component

--- src/components/ui/Brand.tsx
+++ src/components/ui/Brand.tsx
@@ -1,9 +1,9 @@
 import Link from 'next/link';
 
-export default function Brand() {
+export default function Brand({ href = '/' }: { href?: string }) {
   return (
     <h1 className="text-2xl font-extrabold tracking-tighter">
-      <Link href="/">
+      <Link href={href}>
         Biyaheng<span className="text-primary">Tipid</span>
       </Link>
     </h1>
