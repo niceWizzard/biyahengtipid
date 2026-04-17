821ebf040c56da4d09b0e52a41f47e0a83b39023
RIchard Manansala <richard232manansala@gmail.com>
2026-04-17 14:24:08 +0800

    feat: add dynamic metadata in trip page

--- src/app/(main)/trip/[id]/page.tsx
+++ src/app/(main)/trip/[id]/page.tsx
@@ -1,11 +1,26 @@
 import { getTripById } from '@/dal/trip';
 import TripClient from './trip-client';
+import { Metadata } from 'next';
 
-export default async function TripPage({
+export const generateMetadata = async ({
   params,
-}: {
-  params: Promise<{ id: string }>;
-}) {
+}: PageProps<'/trip/[id]'>): Promise<Metadata> => {
+  const { id } = await params;
+  const trip = await getTripById(id);
+
+  if (!trip) {
+    return {
+      title: 'Trip not found',
+    };
+  }
+
+  return {
+    title: `${trip.name} - BiyahengTipid`,
+    description: `Manage your ${trip.name} trip and view stats.`,
+  };
+};
+
+export default async function TripPage({ params }: PageProps<'/trip/[id]'>) {
   const { id } = await params;
   const trip = await getTripById(id);
 
